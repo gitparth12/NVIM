027bf8a4ccd23e0fc4f2c68b421acd5e580d1a2a
Parth Bhargava <41479054+gitparth12@users.noreply.github.com>
2025-03-28 15:40:21 +1100

    Add python stuff back into config, install pylyzer and pylint as libraries as well

--- lua/parth/core/python.lua
+++ lua/parth/core/python.lua
@@ -1,36 +1,38 @@
 -- -- local python_version = "python3.12"
--- local python_version = vim.fn.system("python3 --version"):match("%d+%.%d+")
---
--- local pynvim_env = vim.fn.stdpath("data") .. "/pynvim_env"
--- if not vim.loop.fs_stat(pynvim_env) then
---   vim.fn.system({
---     "python",
---     "-m",
---     "venv",
---     pynvim_env,
---   })
--- end
---
--- local pynvim_python = pynvim_env .. "/bin/python"
---
--- local pynvim_lib = pynvim_env .. "/lib/" .. python_version .. "/site-packages/pynvim"
--- if not vim.loop.fs_stat(pynvim_lib) then
---   vim.fn.system({
---     pynvim_python,
---     "-m",
---     "pip",
---     "install",
---     "pynvim",
---   })
--- end
---
--- vim.g.python_host_prog = pynvim_python
--- vim.g.python3_host_prog = pynvim_python
---
--- if vim.env.VIRTUAL_ENV then
---   local virtual_env_bin = vim.env.VIRTUAL_ENV .. "/bin"
---   vim.env.PATH = virtual_env_bin .. ":" .. vim.env.PATH
--- end
+local python_version = vim.fn.system("python3 --version"):match("%d+%.%d+")
+
+local pynvim_env = vim.fn.stdpath("data") .. "/pynvim_env"
+if not vim.loop.fs_stat(pynvim_env) then
+  vim.fn.system({
+    "python",
+    "-m",
+    "venv",
+    pynvim_env,
+  })
+end
+
+local pynvim_python = pynvim_env .. "/bin/python"
+
+local pynvim_lib = pynvim_env .. "/lib/" .. python_version .. "/site-packages/pynvim"
+if not vim.loop.fs_stat(pynvim_lib) then
+  vim.fn.system({
+    pynvim_python,
+    "-m",
+    "pip",
+    "install",
+    "pynvim",
+    "pylyzer",
+    "pylint",
+  })
+end
+
+vim.g.python_host_prog = pynvim_python
+vim.g.python3_host_prog = pynvim_python
+
+if vim.env.VIRTUAL_ENV then
+  local virtual_env_bin = vim.env.VIRTUAL_ENV .. "/bin"
+  vim.env.PATH = virtual_env_bin .. ":" .. vim.env.PATH
+end
 --
 -- -- if VIRTUAL_ENV is set, remove ~/.pyenv/shims from path
 -- -- if vim.env.VIRTUAL_ENV then
