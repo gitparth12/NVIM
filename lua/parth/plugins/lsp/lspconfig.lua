da44ee4a84b8470af6660ca06baae7165b4f29a4
Parth Bhargava <41479054+gitparth12@users.noreply.github.com>
2025-12-30 15:55:30 +0530

    Fix deprecated sign_define and move definitions in diagnostic.config

--- lua/parth/plugins/lsp/lspconfig.lua
+++ lua/parth/plugins/lsp/lspconfig.lua
@@ -32,7 +32,14 @@ return {
     }
 
     vim.diagnostic.config({
-      signs = true,
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = " ",
+          [vim.diagnostic.severity.WARN] = " ",
+          [vim.diagnostic.severity.INFO] = " ",
+          [vim.diagnostic.severity.HINT] = "󰌵 ",
+        },
+      },
       underline = true,
       virtual_text = false,
       virtual_lines = false,
